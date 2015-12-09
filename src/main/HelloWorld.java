c0627f940daa020e2028d2428cf580d57aa26325
Vijay Mishra <dbazone@gmail.com>
2015-12-09 10:13:24 +0530

    String based practice

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -6,7 +6,16 @@ public class HelloWorld {
 		
 		StringExample se = new StringExample();
 		
-		System.out.println(se.find_last_occurance(args[0], args[1]));
+		String str = "PERSISTENCE is THE KEY TO SUCCESS";
+
+		se.string_to_upper(str);
+		
+
+		//System.out.println(se.string_search(str,"IS"));
+		
+		///System.out.println(se.replace_string("VIJAYAAK", "AY", "I"));
+				
+//		System.out.println(se.find_last_occurance(args[0], args[1]));
 		
 //		System.out.println(se.equals(args[0], args[1]));
 		
--- src/main/StringExample.java
+++ src/main/StringExample.java
@@ -19,4 +19,86 @@ public class StringExample {
 	{
 		return str1.lastIndexOf(str2,3);
 	}
+	
+	public String clean_string(String str1, String str2_to_be_replaced)
+	{
+		String new_str="";
+		
+		new_str = str1.replace(str2_to_be_replaced, "");
+		return new_str;
+		
+	}
+	
+	public String replace_string(String str1, String str2_to_be_replaced, String str3_which_will_replace)
+	{
+		String new_str="";
+		
+		new_str = str1.replace(str2_to_be_replaced, str3_which_will_replace);
+		return new_str;
+		
+	}
+	
+	public String string_reverse(String str)
+	{
+		String str_rev = new StringBuffer(str).reverse().toString();
+				//.reverse().toString();
+	
+		
+		return str_rev;
+		
+		
+//		String str ="PERSISTENCE IS KEY TO SUCCESK";
+//		String str_rev = "";
+//		
+//		int i =str.length();
+//		
+//		
+//		
+//		while (i>0)
+//		{
+//			i--;
+//		//	System.out.println(i);
+//	//		System.out.println(str.substring(28,29));
+//			
+//			str_rev= str_rev + str.substring(i,i+1);
+//			
+//		}
+//		
+//		
+//		System.out.println(str_rev);
+	}
+	
+
+	
+	public int string_search(String str, String what_to_find)
+	{
+				
+		return str.indexOf(what_to_find);
+		
+	}
+	
+	
+	
+	public void string_to_upper(String str)
+	{
+		
+		
+		System.out.println(str.toUpperCase());
+		
+		
+	}
+	
+	public void split_sentence(String str, String delimeter )
+	{
+		
+		String[] str_arr;
+		str_arr= str.split(delimeter);
+		
+				for (String word:str_arr)
+				{
+					System.out.println(word);
+				}
+				
+	}
+	
 }
